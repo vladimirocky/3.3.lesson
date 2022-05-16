120664ce74551422717ac57f58877415525e74bc
A.Vidadi <95567019+Vidadi920@users.noreply.github.com>
2022-05-16 17:51:03 +0300

    Update script.js
    сщккусеув

--- script.js
+++ script.js
@@ -12,114 +12,81 @@
  */
 
 class Person {
-    constructor(name, age, profession) {
-        this.name = name;
-        this.age = age;
-        this.profession = profession;
-    }
+  constructor(name, age, profession) {
+    this.name = name;
+    this.age = age;
+    this.profession = profession;
+  }
 }
 
 const person1 = new Person('Petya', 23, 'bartender');
 const person2 = new Person('Lena', 35, 'painter');
 
 person2.introduceMyself = function () {
-    console.log(
-        `Hello! Me name is ${this.name}.
+  console.log(
+    `Hello! Me name is ${this.name}.
         I'm ${this.age} years old
         I'm a ${this.profession}`
-    );
-}
+  );
+};
 
 // person2.introduceMyself();
 let func = person2.introduceMyself;
-
-person1.introduceMyself = function () {
-    console.log(
-        `Hello! Me name is ${this.name}.
-        I'm ${this.age} years old
-        I'm a ${this.profession}`
-    ); 
-}
-/**
- * ---------------------------------------------------------------------!
- * Тут нужно вызвать func в контексте person1
- * воспользовавшись call, затем apply, затем bind
- * а не переписать туже функцию для person1
- * ---------------------------------------------------------------------!
- */
-person1.introduceMyself();
-
-
+func.call(person1);
+func.apply(person1);
+let func2 = func.bind(person1);
+func2();
 
 //  * ======== 2 ==========
 //  * ВАШ КОД ТУТ
- 
+
 //  * Потчи тоже самое что и в 1 задании
- 
+
 //  * Наследуйте от класса Person два других класса:
 //  * 1. Bartender (для этого класса реализуйте метод introduceMyself как в предыдущем задании)
 class Bartender extends Person {
-    /**
-     * ---------------------------------------------------------------------!
-     * тут неправильный синтаксис у конструктора класса
-     * ---------------------------------------------------------------------!
-     */
-    constructor (name, age, profession)
-    super(name, age, profession)
-    introduceMyself() {
-        super.introduceMyself()
-    }
+  constructor(name, age) {
+    super();
+    this.name = name;
+    this.age = age;
+    this.profession = 'Barthender';
+  }
 }
-const Bart1 = new Bartender('Alex', 26, 'coach');
 //  * 2. Painter
 class Painter extends Person {
-    /**
-     * ---------------------------------------------------------------------!
-     * тут неправильный синтаксис у конструктора класса
-     * ---------------------------------------------------------------------!
-     */
-    constructor (name, age, profession)
-    super(name, age, profession)
-    introduceMyself() {
-        super.introduceMyself()
-    }
+  constructor(name, age) {
+    super();
+    this.name = name;
+    this.age = age;
+    this.profession = 'Painter';
+  }
 }
-const Paint1 = new Painter('Vadim', 19, 'driver');
-let func1 = Paint1.introduceMyself;
+const bart1 = new Bartender('Alex', 26, 'coach');
+const paint1 = new Painter('Vadim', 19, 'driver');
+bart1.introduceMyself = func.bind(bart1);
+bart1.introduceMyself();
+let func5 = func.bind(paint1);
+func5();
 
-Paint1.introduceMyself = function () {
-    console.log(
-        `Hello! Me name is ${this.name}.
-        I'm ${this.age} years old
-        I'm a ${this.profession}`
-    ); 
-}
-Paint1.introduceMyself();
 //  * Создайте экземпляры дочерних классов
-/**
- * ---------------------------------------------------------------------!
- * Где func ? apply ? call? bind?
- * ---------------------------------------------------------------------!
- */
 //  * Скопируйте метод introduceMyself класса Bartender в функцию func
 //  * вызовете функцию func привязав к контексту экземплра класса Painter
- 
 
 // ФЛАГИ ДЕСКРИПТОРОВ СВОЙСТВ, ГЕТТЕРЫ, СЕТТЕРЫ
 const User = {
-    name: 'Vasya',
-    age: 24,
-    password: 'root123',
-    bithdate: '10.10.1990',
-    
-    get fullText() {
-        return `Hi I'm  ${this.name} Me ${this.age} years!`
-    },
-    set changeName(newName) {
-        [this.name] = newName.split(" ");
-    }
+  name: 'Vasya',
+  age: 24,
+  password: 'root123',
+  bithdate: '10.10.1990',
+
+  get fullText() {
+    return `Hi I'm  ${this.name} Me ${this.age} years!`;
+  },
+  set changeName(newName) {
+    [this.name] = newName.split(' ');
+  },
 };
-User.changeName = "Vidadi";
+User.changeName = 'Vidadi';
 console.log(User.name);
 console.log(User.fullText);
 /**
@@ -135,42 +102,37 @@ console.log(User.fullText);
 
 //  * 1. Для свойства name
 //  * флаг configurable установите false
- Object.defineProperty(User, 'name', {
-     configurable: false
- });
+Object.defineProperty(User, 'name', {
+  configurable: false,
+});
 //  * 2. Попробуйте теперь изменить значение name
 //  * (используйте ваш сеттер)
-/**
- * ---------------------------------------------------------------------!
- * что такое change ?
- * ---------------------------------------------------------------------!
- */
- User.change = 'Volodya';
- console.log(User);
+User.changeName = 'Volodya';
+console.log(User);
 //  * 3. Для свойства password
 //  * флаг writable установите в false
 Object.defineProperty(User, 'password', {
-    writable: false
-})
+  writable: false,
+});
 //  * 4. Попробуйте теперь просмотреть значение password
-console.log(User.password)
+console.log(User.password);
 //  * Что в итоге произошло? Почему?
 // Стало не изменным.Потому-что если false, то свойство только для чтения (нельзя
 //     изменить)
-    
+
 //  * 5. Для свойств password и bithdate
 //  * флаг enumerable установите false
- Object.defineProperties(User, {
-     password: {enumerable: false},
-     bithdate: {enumerable: false}
- })
+Object.defineProperties(User, {
+  password: { enumerable: false },
+  bithdate: { enumerable: false },
+});
 //  * 6. В цикле выведете все свойства
 for (let key in User) {
-    console.log(key);
+  console.log(key);
 }
 //  * Что получилось? Почему?
 //  Не вышли ключи password и bithdate потомучто задан флаг enumerable: false
 // вывод свойств User
 for (let key in User) {
-    console.log(Object.getOwnPropertyDescriptor(User, key));
+  console.log(Object.getOwnPropertyDescriptor(User, key));
 }
